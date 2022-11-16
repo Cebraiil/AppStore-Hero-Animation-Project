050cc0ef2752b4fe66b21e222eb931f5f202ddca
Ayman Jibrael <aymanjibrael@outlook.com>
2022-11-16 15:55:15 +0300

    Initial Commit

---
+++ AppStore-Animation-App/AppStore_Animation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStore_Animation_AppApp.swift
+//  AppStore-Animation-App
+//
+//  Created by Ayman on 16.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStore_Animation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStore-Animation-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppStore-Animation-App
+//
+//  Created by Ayman on 16.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
